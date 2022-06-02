894d19f9b6f2fa78457a9871151e51f2ccecc720
Luciano Silva <93166470+LucianoSilva2020@users.noreply.github.com>
2022-06-02 14:12:59 -0300

    Atualização 02

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ menuMobile.addEventListener("click", () => {
     : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle("menu-nav-active");
 });
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
