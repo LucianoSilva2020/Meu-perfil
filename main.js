bfe2cfb24eb5c62719496cdbd38aa29b04e8328c
Luciano Silva <93166470+LucianoSilva2020@users.noreply.github.com>
2022-06-03 15:33:32 -0300

    Atualização final

--- main.js
+++ main.js
@@ -18,3 +18,22 @@ navItem.forEach(item => {
         }
     })
 })
+
+const item = document.querySelectorAll("[data-anime");
+
+const animeScrol = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+    item.forEach(element => {
+        if (windowTop > element.offsetTop){
+            element.classList.add('animate');
+        }else{
+            element.classList.remove("animate");
+        }
+    });
+};
+
+animeScrol();
+
+window.addEventListener("scroll", () => {
+    animeScrol();
+})
